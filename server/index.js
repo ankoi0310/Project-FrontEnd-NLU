fa77125b70d3702a7b48c55c01bc5471b81faa05
huynhvanhuuan <19130003@st.hcmuaf.edu.vn>
2021-05-24 17:01:03 +0700

    update files

---
+++ server/index.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import http from 'http';
+import WebSocket from 'ws';
+
+const port = 2001;
+const server = http.createServer(express);
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', function connection(ws, req) {
+
+    console.log('New connection open!');
+
+    ws.on('message', function(data) {
+        wss.clients.forEach(function each(client) {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+                client.send(data);
+            }
+        })
+    })
+
+    ws.on('close', function() {
+        console.log(`Connection closed!`);
+    })
+})
+
+server.listen(port, function() {
+    console.log(`Server is listening on port ${port}!`);
+})
