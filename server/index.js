3c661dbdfec45b971ff4ef0b95ade0031d3ed7a4
huynhvanhuuan <19130003@st.hcmuaf.edu.vn>
2021-06-28 13:53:02 +0700

    create server

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ import http from 'http';
 import WebSocket from 'ws';
 
 const port = 2001;
-const server = http.createServer(express);
+const server = http.createServer(express());
 const wss = new WebSocket.Server({ server });
 
 wss.on('connection', function connection(ws, req) {
@@ -23,6 +23,9 @@ wss.on('connection', function connection(ws, req) {
     })
 })
 
-server.listen(port, function() {
+server.listen(port, function(err) {
+    if (err) {
+        throw err;
+    }
     console.log(`Server is listening on port ${port}!`);
 })
