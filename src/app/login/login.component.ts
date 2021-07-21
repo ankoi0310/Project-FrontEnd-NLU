d57272f8fe86a4254b082a25c98f37732c109943
huynhvanhuuan <19130003@st.hcmuaf.edu.vn>
2021-07-22 03:20:09 +0700

    add login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { ChatService } from '../chat.service';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+    selector: 'login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+    userLogin: any;
+    passLogin: any;
+    userRegister: any;
+    passRegister: any;
+    constructor(private _chatService: ChatService) { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void { }
 
+    connect = () => {
+        this._chatService.connect();
+    }
+
+    register = () => {
+		this._chatService.register({ "user": this.userRegister, "pass": this.passRegister });
+		this.userRegister = "";
+		this.passRegister = "";
+    }
+
+    login = () => {
+		this._chatService.login({ "user": this.userLogin, "pass": this.passLogin });
+		this.userLogin = "";
+		this.passLogin = "";
+    }
 }
