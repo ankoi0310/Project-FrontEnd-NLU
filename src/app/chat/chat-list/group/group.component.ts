a75a0b0f255e16820f308250477212951f26c6c2
huynhvanhuuan <19130003@st.hcmuaf.edu.vn>
2021-07-31 02:17:11 +0700

    repair group component

--- src/app/chat/chat-list/group/group.component.ts
+++ src/app/chat/chat-list/group/group.component.ts
@@ -1,42 +1,28 @@
 
 import { Component, OnInit } from '@angular/core';
 import { ChatService } from '../../../chat.service';
-//import * as myGlobals from '../../../globals';
 @Component({
     selector: 'group-chat',
     templateUrl: './group.component.html',
     styleUrls: ['./group.component.css'],
 })
 
-export  class GroupComponent implements OnInit {
-    
-   public nameRoom: any;
-   public groupList: any = [];
-    constructor(private _chatService: ChatService) {}
+export class GroupComponent implements OnInit {
+    nameRoom: any;
+    groupList: any;
 
-    ngOnInit(): void {}
+    constructor(private _chatService: ChatService) {
+        this.groupList = _chatService.groupList;
+    }
+
+    ngOnInit(): void { }
 
     createRoom = () => {
-        //myGlobals.nameRoom = this.nameRoom;
         this._chatService.createRoom({ name: this.nameRoom });
-       
-     
-        if(status == 'error' ){;
-        this.groupList.push({
-            name: this.nameRoom,
-            mes: 'message',
-        });
-        this.nameRoom = '';
-    }
     };
+
     joinRoom = () => {
         this._chatService.joinRoom({ name: this.nameRoom });
-
-        this.groupList.push({
-            name: this.nameRoom,
-            mes: 'message',
-        });
-        this.nameRoom = '';
     };
-    
+
 }
