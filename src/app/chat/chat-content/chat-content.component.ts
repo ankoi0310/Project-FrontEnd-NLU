2338cce4f740631a2853ef691b169e26e219eae5
huynhvanhuuan <19130003@st.hcmuaf.edu.vn>
2021-08-02 16:07:43 +0700

    Câp nhât chưc năng

--- src/app/chat/chat-content/chat-content.component.ts
+++ src/app/chat/chat-content/chat-content.component.ts
@@ -1,14 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ChatService } from 'src/app/chat.service';
 @Component({
-  selector: 'app-chat-content',
-  templateUrl: './chat-content.component.html',
-  styleUrls: ['./chat-content.component.css']
+	selector: 'app-chat-content',
+	templateUrl: './chat-content.component.html',
+	styleUrls: ['./chat-content.component.css']
 })
 export class ChatContentComponent implements OnInit {
+	to: any;
+	message: any
+	chatHistory: any[] = [];
 
-    constructor() { }
+	constructor(private _chatService: ChatService) {
+		this.chatHistory = this._chatService.chatHistory;
+	}
 
-    ngOnInit(): void {
+	ngOnInit(): void { }
 
-    }
+	sendMessage = () => {
+		this._chatService.chatToPeople(this.message);
+		this.message = '';
+	}
 }
