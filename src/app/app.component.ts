891f5f147be7a1165eb2ff29c0ed4d8812f4635b
huynhvanhuuan <19130003@st.hcmuaf.edu.vn>
2021-08-02 16:12:29 +0700

    format code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { ChatService } from './chat.service';
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
-    
+
 export class AppComponent implements OnInit {
 
-    constructor() { }
-    
-    ngOnInit(): void {}
+	constructor() { }
 
-    title = 'Project-FrontEnd-NLU';
-    
+	ngOnInit(): void { }
+
+	title = 'Project-FrontEnd-NLU';
+
 }
