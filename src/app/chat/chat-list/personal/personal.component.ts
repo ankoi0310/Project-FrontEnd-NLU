fc4befaa81c1a14894553fb3bf73fa83a438d615
huynhvanhuuan <19130003@st.hcmuaf.edu.vn>
2021-08-02 16:08:51 +0700

    them chuc nang show click de lay du lieu chat

--- src/app/chat/chat-list/personal/personal.component.ts
+++ src/app/chat/chat-list/personal/personal.component.ts
@@ -1,33 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { ChatService } from 'src/app/chat.service';
 
 @Component({
-  selector: 'personal-chat',
-  templateUrl: './personal.component.html',
-  styleUrls: ['./personal.component.css']
+	selector: 'personal-chat',
+	templateUrl: './personal.component.html',
+	styleUrls: ['./personal.component.css']
 })
 export class PersonalComponent implements OnInit {
 
-  constructor() { }
+	username: any;
+	peopleList: any[] = [];
 
-  ngOnInit(): void {
-  }
+	constructor(private _chatService: ChatService) {
+		this.peopleList = _chatService.peopleList;
+	}
 
-  personalList: any[] = [
-    {
-      "name": "a",
-      "mes": "message",
-    },
-    {
-      "name": "b",
-      "mes": "message",
-    },
-    {
-      "name": "c",
-      "mes": "message",
-    },
-    {
-      "name": "d",
-      "mes": "message",
-    }
-  ];
+	ngOnInit(): void {
+	}
+
+	findPeople = () => {
+		if (this.username) {
+			this._chatService.findPeople({ "user": this.username });
+			this.username = "";
+		} else {
+			alert('Please enter the name');
+		}
+	}
+
+	getPeopleChatMessage = (name: any) => {
+		this._chatService.getPeopleChatMessage(name);
+	}
 }
