d996f3b700bf9a4a9f519d093e0c43fd11e86815
huynhvanhuuan <19130003@st.hcmuaf.edu.vn>
2021-09-08 10:06:53 +0700

    Bat loi chat va cap nhat, phan loai chuc nang

--- src/app/chat/chat-content/chat-content.component.ts
+++ src/app/chat/chat-content/chat-content.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ChatService } from 'src/app/chat.service';
 @Component({
 	selector: 'app-chat-content',
@@ -6,27 +6,41 @@ import { ChatService } from 'src/app/chat.service';
 	styleUrls: ['./chat-content.component.css']
 })
 export class ChatContentComponent implements OnInit {
-	to: any;
+	chatHistory: any = []
+	type: string = ""
 	message: any
-	chatHistory: any[] = [];
 
 	constructor(private _chatService: ChatService) {
+		this.type = this._chatService.type;
 		this.chatHistory = this._chatService.chatHistory;
 	}
 
 	ngOnInit(): void { }
 
 	sendMessage = () => {
+		let to = document.getElementById('to');
 		let name = document.getElementById('user');
-		if (name) {
-			this.chatHistory.push({
-				'mes': this.message,
-				'name': name,
-				'to': this.to,
-				'className': 'message send',
-			});
+		if (to && to.textContent != "") {
+			if (this.message != null && this.message != "") {
+				if (name) {
+					this.chatHistory.push({
+						'mes': this.message,
+						'name': name.textContent,
+						'to': to.textContent,
+						'className': 'message send',
+					});
+				}
+				if (this._chatService.type == 'people') {
+					this._chatService.chatToPeople({ to: to.textContent, message: this.message });
+				} else {
+					this._chatService.chatToRoom({ to: to.textContent, message: this.message });
+				}
+			} else {
+				alert('Please type a message');
+			}
+		} else {
+			alert('Please choose a person to send');
 		}
-		this._chatService.chatToPeople(this.message);
 		this.message = '';
-	} 
+	}
 }
